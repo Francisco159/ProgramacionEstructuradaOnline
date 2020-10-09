28d1b5d0ac8681c701dff3e50deae85833a2a019
Francisco159 <javier.671634@gmail.com>
2020-10-08 23:13:33 -0500

    Programa que imprime un cuadrado, donde declaramos la base y la altura en una estructura (struct)

---
+++ Tarea6/Cuadrado_con_struct.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+
+struct cuadrilatero{
+	int base;
+	int altura;
+};
+
+int main()
+{
+	int i,j;
+	struct cuadrilatero cuadrilatero1;
+	cuadrilatero1.base=10;
+	cuadrilatero1.altura = 5;
+	
+	for(i = 1; i <= cuadrilatero1.altura; i++)
+	{
+		for(j = 1; j <= cuadrilatero1.base; j++)
+		{
+			if(i == 1 && j == 1 || i == cuadrilatero1.altura && j == 1 
+			   || i == 1 && j == cuadrilatero1.base || i == cuadrilatero1.altura && j == cuadrilatero1.base)
+			{
+				printf("+");
+			}
+			else
+			{
+				if(j == 1 || j == cuadrilatero1.base)
+				{
+					printf("|");
+				}
+				else
+				{
+					if((i == 1 && j >= 2 && j < cuadrilatero1.base) 
+						|| (i == cuadrilatero1.altura && j >= 2 && j < cuadrilatero1.base))
+					{
+						printf("-");
+					}
+					else
+					{
+						printf(" ");
+					}
+				}
+			}
+		}
+		printf("\n");
+	}
+	
+	return 1;
+}
